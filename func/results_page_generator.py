460d16b2ab59c38ff48e584c79e2f96774e37253
Ted Mathew dela Cruz <tedmdelacruz@gmail.com>
2019-02-23 09:56:43 +0800

    Fixed generation of analysis results page

--- func/results_page_generator.py
+++ func/results_page_generator.py
@@ -10,6 +10,7 @@ from jinja2 import Template
 HASHTAG = os.environ['hashtag']
 AWS_REGION = os.environ['aws_region']
 STATIC_PAGE_BUCKET = os.environ['results_page_bucket']
+ANALYZED_TWEETS_TABLE = os.environ['analyzed_tweets_table']
 DEBUG = False
 
 curr_path = os.path.dirname(os.path.realpath(__file__))
@@ -17,16 +18,14 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    if 'Records' not in event:
-        logger.info('No records found')
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(ANALYZED_TWEETS_TABLE)
+    results = table.scan()
+    if 'Items' not in results:
+        logger.info('No items found')
         return
-    
-    tweets = [
-        unmarshall(record['dynamodb']['NewImage'])
-        for record in event['Records']
-        if record['eventName'] == 'INSERT'
-    ]
 
+    tweets = results['Items']
     template_file = open(os.path.join(curr_path, 'tmpl', 'index.html'))
     template = Template(template_file.read())
     logger.info(tweets)
@@ -37,6 +36,7 @@ def lambda_handler(event, context):
             os.path.join(curr_path, 'rendered_page.html'))
         outfile = open(outfile_path, 'w')
         outfile.write(rendered_page)
+        print(f'Generated: {outfile_path}')
         return
 
     s3 = boto3.client('s3', region_name=AWS_REGION)
